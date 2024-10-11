d432246d7311bde35e6ec924813a19444df6e162
Vlada-en <Edenisova73@mail.ru>
2024-10-11 13:23:11 +0300

    lesson 2: A, B, C, D

--- Занятие 2/Практические задания/!ProgramTasks/C/Program.cs
+++ Занятие 2/Практические задания/!ProgramTasks/C/Program.cs
@@ -7,18 +7,14 @@
             Console.WriteLine(MiddleOf(5, 0, 100)); // => 5
             Console.WriteLine(MiddleOf(12, 12, 11)); // => 12
             Console.WriteLine(MiddleOf(1, 1, 1)); // => 1
-            Console.WriteLine(MiddleOf(2, 3, 2));
-            Console.WriteLine(MiddleOf(8, 8, 8));
-            Console.WriteLine(MiddleOf(5, 0, 1));
+            Console.WriteLine(MiddleOf(2, 3, 2)); // => 2
+            Console.WriteLine(MiddleOf(8, 8, 8)); // => 8
+            Console.WriteLine(MiddleOf(5, 0, 1)); // => 1
         }
 
         public static int MiddleOf(int a, int b, int c)
         {
-            if (a > b)
-                if (b > c) return b;
-                else if (a > c) return ...
-            else
-                ...
+            return a + b + c - Math.Min(Math.Min(a, b), c) - Math.Max(Math.Max(a, b), c);
         }
     }
 }
