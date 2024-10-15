a5b4d31be1d567ca7c20838247d08316aa7add35
GogolMogol223 <lefloppapelmeshkov@vk.com>
2024-10-15 12:46:31 +0300

    Update Program.cs

--- Занятие 1/Практические задания/!ProgramTasks/C/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/C/Program.cs
@@ -6,9 +6,9 @@
         {
             double pi = Math.PI;
             int tenThousand = 10000L;
-            float tenThousandPi = pi * tenThousand;
-            int roundedTenThousandPi = tenThousandPi;
-            int integerPartOfTenThousandPi = tenThousandPi;
+            float tenThousandPi = tenThousand * (float)pi;
+            int roundedTenThousandPi = (int)Math.Round(tenThousandPi);
+            int integerPartOfTenThousandPi = (int)Math.Floor(tenThousandPi);
             Console.WriteLine(integerPartOfTenThousandPi);
             Console.WriteLine(roundedTenThousandPi);
         }
