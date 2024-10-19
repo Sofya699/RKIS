c5a2a53785373514ee34d58be92945bad8f2dfc9
Evgeny <pekarink@gmail.com>
2024-10-19 13:50:02 +0300

    Update Program.cs

--- Занятие 1/Практические задания/!ProgramTasks/F/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/F/Program.cs
@@ -1,10 +1,11 @@
+using System;
 ﻿namespace F
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var a = 5; // ← исправьте эту строку
+            double a = 5; // ← исправьте эту строку
             a += 0.5;
             Console.WriteLine(a);
         }
