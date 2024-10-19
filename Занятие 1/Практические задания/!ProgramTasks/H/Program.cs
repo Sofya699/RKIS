9dfede874478c8eb0ae299f6f8fcb37a9ebfc022
Evgeny <pekarink@gmail.com>
2024-10-19 14:02:03 +0300

    Update Program.cs

--- Занятие 1/Практические задания/!ProgramTasks/H/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/H/Program.cs
@@ -1,7 +1,20 @@
-﻿namespace H
+using System;
+namespace H
 {
     internal class Program
     {
+        // Метод для вычисления квадрата числа
+        private static int GetSquare(int number)
+        {
+            return number * number;
+        }
+
+        // Метод для вывода числа на консоль
+        private static void Print(int value)
+        {
+            Console.WriteLine(value);
+        }
+
         static void Main(string[] args)
         {
             Print(GetSquare(42));
