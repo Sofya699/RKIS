a863a5e87f1bce1c844c8d08e9987ad9724f47f5
GogolMogol223 <lefloppapelmeshkov@vk.com>
2024-10-20 20:18:23 +0300

    Update PluralizeTask.cs

--- Занятие 2/Практические задания/Pluralize/PluralizeTask.cs
+++ Занятие 2/Практические задания/Pluralize/PluralizeTask.cs
@@ -4,7 +4,13 @@ public static class PluralizeTask
 {
 	public static string PluralizeRubles(int count)
 	{
-		// Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
-		return "руб.";
+	string str = "рублей";
+            if (count % 10 == 1) 
+                str = "рубль";
+            if (count % 10 >= 2 && count % 10 <= 4) 
+                str = "рубля";
+            if (count % 100 >= 11 && count % 100 <= 20) 
+                str = "рублей";
+            return str;
 	}
 }
