b69016decdba933ce88924ab4dcf38df251f728a
GogolMogol223 <lefloppapelmeshkov@vk.com>
2024-10-20 11:24:18 +0300

    Update Program.cs

--- Занятие 1/Практические задания/!ProgramTasks/J/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/J/Program.cs
@@ -2,12 +2,12 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string args)
         {
-            Console.WriteLine{ 'Hello, World!'};
-            var numher = 5,5;
+            Console.WriteLine{ "Hello, World!"};
+            var number = 5,5;
             number += 7;
-            Console.Writeline(number):
+            Console.Writeline(number);
         }
     }
 }
