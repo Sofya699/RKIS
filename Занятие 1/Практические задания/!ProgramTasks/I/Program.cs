6f97b69ff7ee263a6788b3a0ccf671dbe63001ea
Evgeny <pekarink@gmail.com>
2024-10-22 23:57:18 +0300

    Update Program.cs

--- Занятие 1/Практические задания/!ProgramTasks/I/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/I/Program.cs
@@ -1,17 +1,28 @@
-﻿namespace I
+using System;
+
+namespace I
 {
     internal class Program
     {
         static string GetLastHalf(string text)
         {
-            ...
+            
+            int startIndex = text.Length / 2;
+
+        
+            {
+                startIndex++;
+            }
+
+            
+            return text.Substring(startIndex);
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine(GetLastHalf("I love CSharp!"));
-            Console.WriteLine(GetLastHalf("1234567890"));
-            Console.WriteLine(GetLastHalf("до ре ми фа соль ля си"));
+            Console.WriteLine(GetLastHalf("I love CSharp!")); // "CSharp!"
+            Console.WriteLine(GetLastHalf("1234567890"));     // "567890"
+            Console.WriteLine(GetLastHalf("до ре ми фа соль ля си")); // "фа соль ля си"
         }
     }
 }
