68a5aea8d04de52a94f207ee13f4d660b2310e41
Daria Kolmuk <nross.dasha@bk.ru>
2024-10-25 23:58:52 +0300

    lesson 1:make A

--- Занятие 1/Практические задания/!ProgramTasks/A/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/A/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            // пишите код тут
-        }
+            Console.Writeline("The first step!");
+            Console.ReadKey():
+        }    
     }
 }
