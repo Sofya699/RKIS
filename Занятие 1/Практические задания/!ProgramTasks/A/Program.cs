70d19f0db327506d48b3076b9a7b77c4db21c8a6
Evgeny <pekarink@gmail.com>
2024-10-26 12:51:03 +0300

    1.A

--- Занятие 1/Практические задания/!ProgramTasks/A/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/A/Program.cs
@@ -1,11 +1,12 @@
 using System;
+
 namespace A
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-        console.WriteLine("The first step!");    // пишите код тут
+            Console.WriteLine("The first step!"); // пишите код тут
         }
     }
 }
