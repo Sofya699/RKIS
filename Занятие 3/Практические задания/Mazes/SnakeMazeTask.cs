531e2d9ae318d75944e589540e2419442a632ef1
Roman <karosen1448@gmail.com>
2024-10-26 13:44:43 +0300

    lesson 3: make Maze

--- Занятие 3/Практические задания/Mazes/SnakeMazeTask.cs
+++ Занятие 3/Практические задания/Mazes/SnakeMazeTask.cs
@@ -2,8 +2,43 @@
 
 public static class SnakeMazeTask
 {
-    public static void MoveOut(Robot robot, int width, int height)
+public static class SnakeMazeTask
     {
+        public static void MoveOut(Robot robot, int width, int height)
+        {
+            var steps = width - 3;
+            while (true)
+            {
+                MoveTo("right", robot, steps);
+                MoveTo("down", robot);
+                MoveTo("left", robot, steps);
+                if (robot.Finished) break;
+                MoveTo("down", robot);
+            }
+        }
 
-    }
+        private static void MoveTo(string direction, Robot robot, int steps ? )
+        {
+            if (direction == "right")
+            {
+                for (var i = 0; i < steps; i++)
+                {
+                    robot.MoveTo(Direction.Right);
+                }
+            }
+            if (direction == "left")
+            {
+                for (var i = 0; i < steps; i++)
+                {
+                    robot.MoveTo(Direction.Left);
+                }
+            }
+            if (direction == "down")
+            {
+                for (var i = 0; i < 2; i++)
+                {
+                    robot.MoveTo(Direction.Down);
+                }
+            }
+        }
 }
