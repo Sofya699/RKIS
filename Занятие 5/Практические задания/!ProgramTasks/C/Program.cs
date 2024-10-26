992f649934d236e800da692b261e0f0bf03f8188
onotolewasserman <alwm2006@hotmail.com>
2024-10-26 10:22:18 +0300

    .

--- Занятие 5/Практические задания/!ProgramTasks/C/Program.cs
+++ Занятие 5/Практические задания/!ProgramTasks/C/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace C
+﻿using System.Text.RegularExpressions;
+
+namespace C
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input = "США: 329957441, Франция - 68859599, Индия; 1375100000, Япония 125950000";
+
+            Console.WriteLine("Оригинальная строка:");
+            Console.WriteLine(input);
+
+            string pattern = @"[ ::,-]+";
+            string output = Regex.Replace(input, pattern, "\t");
+
+            Console.WriteLine("\nПреобразованная строка:");
+            Console.WriteLine(output);
+
         }
     }
 }
