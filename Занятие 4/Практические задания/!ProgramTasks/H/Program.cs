8030cd8cf0674f9403b183bba36713003dd7afec
s7v7nn <narmincik989@gmail.com>
2024-10-27 22:15:38 +0300

    lesson4:program tasks

--- Занятие 4/Практические задания/!ProgramTasks/H/Program.cs
+++ Занятие 4/Практические задания/!ProgramTasks/H/Program.cs
@@ -8,14 +8,12 @@
             Cross,
             Circle
         }
-
         public enum GameResult
         {
             CrossWin,
             CircleWin,
             Draw
         }
-
         public static void Main()
         {
             Run("XXX OO. ...");
@@ -31,6 +29,55 @@
         public static GameResult GetGameResult(Mark[,] field)
         {
             ...
+            bool crossWin = CheckWinner(field, Mark.Cross);
+            bool circleWin = CheckWinner(field, Mark.Circle);
+
+            if (crossWin && circleWin)
+            {
+                return GameResult.Draw;
+            }
+            else if (crossWin)
+            {
+                return GameResult.CrossWin;
+            }
+            else if (circleWin)
+            {
+                return GameResult.CircleWin;
+            }
+            else
+            {
+                return GameResult.Draw; // НИЧЬЯ 
+            }
+        }
+
+        private static bool CheckWinner(Mark[,] field, Mark marker)
+        {
+            // СТРОКИ
+            for (int i = 0; i < 3; i++)
+            {
+                if (field[i, 0] == marker && field[i, 1] == marker && field[i, 2] == marker)
+                {
+                    return true;
+                }
+            }
+
+            // СТОЛБЦЫ
+            for (int i = 0; i < 3; i++)
+            {
+                if (field[0, i] == marker && field[1, i] == marker && field[2, i] == marker)
+                {
+                    return true;
+                }
+            }
+
+            // Д
+            if ((field[0, 0] == marker && field[1, 1] == marker && field[2, 2] == marker) ||
+                (field[0, 2] == marker && field[1, 1] == marker && field[2, 0] == marker))
+            {
+                return true;
+            }
+
+            return false;
         }
 
         private static void Run(string description)
@@ -39,7 +86,6 @@
             Console.WriteLine(GetGameResult(CreateFromString(description)));
             Console.WriteLine();
         }
-
         private static Mark[,] CreateFromString(string str)
         {
             var field = str.Split(' ');
